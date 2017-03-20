33e1c73d51a58c4d3c97f5df59f14eb11910c4f2
HuanSheng <greentea122622@gmail.com>
2017-03-20 13:21:17 +0800

    TestPorject Upload

---
+++ TestPorject/Assets/Object.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public GameObject Cube;
+
+	void Update () {
+
+        float xspeed = Input.GetAxis("Horizontal");
+        float yspeed = Input.GetAxis("Vertical");
+
+        Cube.transform.Translate(xspeed * Time.deltaTime, yspeed * Time.deltaTime, 0);
+	}
+}
